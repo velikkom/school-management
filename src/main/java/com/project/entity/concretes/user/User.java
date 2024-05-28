808ba3fa0a1467cca1644e9b392f72b40d4e60be
Veli Kara <113050906+velikkom@users.noreply.github.com>
2024-05-28 19:19:29 +0300

    Entity package for all entity

--- src/main/java/com/project/entity/concretes/user/User.java
+++ src/main/java/com/project/entity/concretes/user/User.java
@@ -1,19 +1,24 @@
 package com.project.entity.concretes.user;
 
-
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.project.entity.concretes.business.LessonProgram;
+
+import com.project.entity.concretes.business.StudentInfo;
 import com.project.entity.enums.Gender;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import javax.persistence.*;
 import java.time.LocalDate;
-import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Set;
 
 @Entity
 @Table(name = "t_user")
+
+@Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder(toBuilder = true)
@@ -23,26 +28,23 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-
     @Column(unique = true)
     private String username;
 
     @Column(unique = true)
     private String ssn;
 
-
     private String name;
 
     private String surname;
 
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
-    private LocalDate birthDate;
+    private LocalDate birthDay;
 
     private String birthPlace;
 
     @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private String password;
-
 
     @Column(unique = true)
     private String phoneNumber;
@@ -50,64 +52,51 @@ public class User {
     @Column(unique = true)
     private String email;
 
-
-    private boolean built_in;
+    private Boolean built_in;
 
     private String motherName;
+
     private String fatherName;
+
     private int studentNumber;
+
     private boolean isActive;
-    private Boolean isAdviser;
-    private Long advisorTeacherId;
+
+    private Boolean isAdvisor;
+
+    private Long advisorTeacherId; // bu Ogrenciler icin lazim, kendi rehber ogretmeninin
+    // id si buraya yazilacak
 
     @Enumerated(EnumType.STRING)
-    private Gender gender;
+    private Gender gender; // MALE , male , Male , MalE
 
     @OneToOne
     @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private UserRole userRole;
 
+    @OneToMany(mappedBy = "teacher", cascade = CascadeType.REMOVE)
+    private List<StudentInfo> studentInfos;
 
+    @JsonIgnore
+    @ManyToMany
+    @JoinTable(
+            name = "user_lessonprogram",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "lesson_program_id")
+    )
+    private Set<LessonProgram> lessonsProgramList;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+//    @JsonIgnore
+//    @ManyToMany
+//    @JoinTable(
+//            name ="meet_student_table",
+//            joinColumns = @JoinColumn(name = "student_id"),
+//            inverseJoinColumns = @JoinColumn(name = "meet_id")
+//    )
+//    private List<Meet> meetList;
 
 }
+
+
+
+
